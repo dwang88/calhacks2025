0a4bc71b30b3ba198e47a4c256ae96a6153081cf
Aarav Navani <arav.navani@gmail.com>
2025-06-21 17:13:46 -0700

    organize code + frontend

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import AI_Prompt from "../components/kokonutui/ai-prompt"
-
-export default function SyntheticV0PageForDeployment() {
-  return <AI_Prompt />
-}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import AI_Prompt from "../components/kokonutui/ai-prompt"
+
+export default function SyntheticV0PageForDeployment() {
+  return <AI_Prompt />
+}
